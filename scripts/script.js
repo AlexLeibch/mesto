f966de6bcf805e2bcb33fb65a6f6c3730a92556e
Alexanrd Leibch <71503887+DeadlyVi@users.noreply.github.com>
2021-05-19 12:23:33 +0200

    fix: добавить модификаторы для поиска элементов в скрипте

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,8 @@ let popup = document.querySelector('.popup');
 let username = document.querySelector('.profile__username');
 let description = document.querySelector('.profile__user-description');
 let formElement = document.querySelector('.popup__input');
-let nameInput = document.querySelector('.popup__field_name');
-let jobInput = document.querySelector('.popup__field_description');
+let nameInput = document.querySelector('.popup__field_type_name');
+let jobInput = document.querySelector('.popup__field_type_description');
 
 function openPopup (evt) {
     popup.classList.add('popup_opened')
