7c050fdcc2c75460a5acb1ba06851a4019bc2d8c
Alexanrd Leibch <71503887+DeadlyVi@users.noreply.github.com>
2021-07-22 18:44:12 +0200

    creating Api.js

---
+++ src/components/Api.js
@@ -0,0 +1,44 @@
+ export default class Api {
+     constructor({adress, token}) {
+        this._token = token
+        this._adress = adress
+     }
+
+     getInitialCards() {
+         return fetch(`${this._adress}/cards`, {
+             headers: {
+                authorization: this._token
+             }
+         })
+         .then(response => response.ok
+            ? response.json()
+            : Promise.reject(`Произошла ошибка:  ${response.status}`))
+     }
+
+     getUserInfo() {
+         return fetch(`${this.adress}/users/me`, {
+             headers: {
+                authorization: this._token
+             }
+         })
+         .then(response => response.ok
+            ? response.json()
+            : Promise.reject(`Произошла ошибка: ${response.status}`))
+     }
+
+     editUserInfo(user, description) {
+         return fetch(`${this.adress}/users/me`, {
+             headers: {
+                 authorization: this._token,
+                 'Content-type': 'application/json'
+             },
+             body: JSON.stringify {
+                 user: user,
+                 about: description,                 
+             })
+             .then(response => response.ok
+                ? response.json()
+                : Promise.reject(`Произошла ошибка: ${response.status}`))
+         }
+     }
+ }
