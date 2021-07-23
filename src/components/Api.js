4102249200cf5db00e156091d6507d9f54405964
Alexanrd Leibch <71503887+DeadlyVi@users.noreply.github.com>
2021-07-23 12:02:31 +0200

    finish api

--- src/components/Api.js
+++ src/components/Api.js
@@ -41,4 +41,68 @@
                 : Promise.reject(`Произошла ошибка: ${response.status}`))
          })
      }
- }
+
+     addCard(name, link) {
+         return fetch(`${this._adress}/cards`, {
+             method: 'POST',
+             headers: {
+                 authorization: this._token,
+                 'Content-type': 'application/json'
+             },
+             body: JSON.stringify({
+                 name: name,
+                 link: link
+             })
+         }).then(response => response.ok
+            ? response.json()
+            : Promise.reject(`Произошла ошибка: ${response.status}`))
+     }
+
+     editUserAvatar(url) {
+        return fetch(`${this._adress}/users/me/avatar`, {
+            method: 'PATCH',
+            headers: {
+                authorization: this._token,
+                'Content-type' : 'application/json'
+            },
+            body: JSON.stringify({
+                avatar: url
+            })
+        }).then(response => response.ok
+            ? response.json()
+            : Promise.reject(`Произошла ошибка: ${response.status}`))
+     }
+
+     likeCard(cardId) {
+         return fetch(`${this._adress}/cards/likes/${cardId}`, {
+             method: 'PUT',
+             headers: {
+                 authorization: this._token
+             }
+         }).then(response => response.ok
+            ? response.json()
+            : Promise.reject(`Ошибка ${response.status}`))
+     }
+
+     dislikeCard(cardId) {
+         return fetch(`${this._adress}/cards/likes/${cardId}`, {
+             method: 'DELETE',
+             headers: {
+                 authorization: this._token
+             }
+         }).then(response => response.ok
+            ? response.json()
+            : Promise.reject(`Произошла ошибка: ${response.status}`))
+     }
+
+     removeCard(cardId) {
+         return fetch(`${this._adress}/cards/${cardId}`, {
+             method: 'DELETE',
+             headers: {
+                 authorization: this._token
+             }
+         }).token(response => response.ok
+            ? response.json()
+            : Promise.reject(`Произошла ошибка: ${response.status}`))
+     }
+}
