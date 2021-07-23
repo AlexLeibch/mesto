cac9976c3470ef8866db9b675fbf3b549b99fe98
Alexanrd Leibch <71503887+DeadlyVi@users.noreply.github.com>
2021-07-23 18:09:53 +0200

    trying to make it work

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,107 +1,93 @@
  export default class Api {
-     constructor({adress, token}) {
-        this._token = token
-        this._adress = adress
+     constructor(options) {
+        this._options = options
      }
 
      getInitialCards() {
-         return fetch(`${this._adress}/cards`, {
-             headers: {
-                authorization: this._token
-             }
-         })
+         return fetch('https://mesto.nomoreparties.co/v1/cohort-26/cards', this._options)
          .then(response => response.ok
             ? response.json()
-            : Promise.reject(`Произошла ошибка:  ${response.status}`))
+            : Promise.reject(`Произошла ошибка:  ${response.status}`)
+            )
      }
 
      getUserInfo() {
-         return fetch(`${this.adress}/users/me`, {
-             headers: {
-                authorization: this._token
-             }
-         })
+         return fetch('https://mesto.nomoreparties.co/v1/cohort-26/users/me', this._options)
          .then(response => response.ok
             ? response.json()
-            : Promise.reject(`Произошла ошибка: ${response.status}`))
+            : Promise.reject(`Произошла ошибка: ${response.status}`)
+            )
      }
 
-     editUserInfo(user, description) {
-         return fetch(`${this.adress}/users/me`, {
-             headers: {
-                 authorization: this._token,
-                 'Content-type': 'application/json'
-             },
-             body: JSON.stringify ({
-                 user: user,
-                 about: description,                 
-             })
-             .then(response => response.ok
-                ? response.json()
-                : Promise.reject(`Произошла ошибка: ${response.status}`))
-         })
-     }
-
-     addCard(name, link) {
-         return fetch(`${this._adress}/cards`, {
-             method: 'POST',
-             headers: {
-                 authorization: this._token,
-                 'Content-type': 'application/json'
-             },
-             body: JSON.stringify({
-                 name: name,
-                 link: link
-             })
-         }).then(response => response.ok
+     editUserInfo(inputValue) {
+         const newOptions = {
+             ...this._options,
+             body: JSON.stringify(inputValue),
+             method: 'PATCH',
+         }
+         return fetch('https://mesto.nomoreparties.co/v1/cohort-26/users/me', newOptions)
+         .then(response => response.ok
             ? response.json()
-            : Promise.reject(`Произошла ошибка: ${response.status}`))
+            : Promise.reject(`Произошла ошибка: ${response.status}`)
+            )
      }
 
-     editUserAvatar(url) {
-        return fetch(`${this._adress}/users/me/avatar`, {
-            method: 'PATCH',
-            headers: {
-                authorization: this._token,
-                'Content-type' : 'application/json'
-            },
-            body: JSON.stringify({
-                avatar: url
-            })
-        }).then(response => response.ok
+     addCard(inputValue) {
+        const newOptions = {
+            ...this._options,
+            body: JSON.stringify(inputValue),
+            method: 'POST'
+        }
+        return fetch('https://mesto.nomoreparties.co/v1/cohort-26/cards', newOptions)
+        .then(response => response.ok
             ? response.json()
-            : Promise.reject(`Произошла ошибка: ${response.status}`))
+            : Promise.reject(`Произошла ошибка: ${response.status}`)
+            )
+     }
+
+     editUserAvatar(inputsValue) {
+        const newOptions = {
+            ...this._options,
+            body: JSON.stringify(inputsValue),
+            method: 'PATCH'
+        }
+        return fetch('https://mesto.nomoreparties.co/v1/cohort-26/user/me/avatar', newOptions)
+        .then(response => response.ok
+            ? response.json()
+            : Promise.reject(`Произошла ошибка: ${response.status}`)
+            )
      }
 
      likeCard(cardId) {
-         return fetch(`${this._adress}/cards/likes/${cardId}`, {
+         const newOptions = {
+             ...this._options,
              method: 'PUT',
-             headers: {
-                 authorization: this._token
-             }
-         }).then(response => response.ok
+         }
+         return fetch(`https://mesto.nomoreparties.co/v1/cohort-26/cards/${cardId}`, newOptions)
+         .then(response => response.ok
             ? response.json()
-            : Promise.reject(`Ошибка ${response.status}`))
+            : Promise.reject(`Произошла Ошибка: ${response.status}`)
+            )
      }
 
      dislikeCard(cardId) {
-         return fetch(`${this._adress}/cards/likes/${cardId}`, {
-             method: 'DELETE',
-             headers: {
-                 authorization: this._token
-             }
-         }).then(response => response.ok
+        const newOptions = {
+            ...this._options,
+            method: 'DELETE',
+        }
+        return fetch(`https://mesto.nomoreparties.co/v1/cohort-26/cards/${cardId}`, newOptions) 
+        .then(response => response.ok
             ? response.json()
             : Promise.reject(`Произошла ошибка: ${response.status}`))
      }
 
      removeCard(cardId) {
-         return fetch(`${this._adress}/cards/${cardId}`, {
+         const newOptions = {
+             ...this._options,
              method: 'DELETE',
-             headers: {
-                 authorization: this._token
-             }
-         }).token(response => response.ok
+         }
+         return fetch(`https://mesto.nomoreparties.co/v1/cohort-26/cards/${cardId}`, newOptions)
+         .then(response => response.ok
             ? response.json()
             : Promise.reject(`Произошла ошибка: ${response.status}`))
      }
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,19 +1,28 @@
 export default class UserInfo {
-    constructor({userName, userDescription}) {
+    constructor({userName, userDescription, profileAvatar}) {
         this._userName = document.querySelector(userName)
         this._userDescription = document.querySelector(userDescription)
+        this._profileAvatar = document.querySelector(profileAvatar)
     }
 
     getUserInfo() {
         const userValue = {
             name: this._userName.textContent,
-            description: this._userDescription.textContent
+            description: this._userDescription.textContent,
+            avatar: this._profileAvatar.src
         }
           return userValue;
     }
 
-    setUserInfo({name, description}) {
-        this._userName.textContent = name
-        this._userDescription.textContent = description 
+    setUserInfo({name, description, avatar}) {
+        if (name) {
+            this._userName.textContent = name;
+        }
+        if (description) {
+            this._userDescription.textcontent = description
+        }
+        if (avatar) {
+            this._profileAvatar.src = avatar
+        }
     }
 }
